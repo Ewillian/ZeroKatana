92d4c07c0312a971f52ccd5c2fea3aafe4a399d9
Ewillian <guillaume.lecoq@ynov.com>
2021-03-01 16:57:17 +0100

    Working menu button animation
    + Button controller script
    + Button Script
    + Cleaning project

---
+++ Assets/Scripts/UI/Menu/MenuController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int index;
+
+    public bool keyDown;
+
+    public int maxIndex = 2;
+
+    //audiosource
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(index);
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            if (!keyDown)
+            {
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    if (index < maxIndex)
+                    {
+                        index++;
+                    }
+                    else
+                    {
+                        index = 0;
+                    }
+                }
+                else if (Input.GetAxis("Vertical") > 0)
+                {
+                    if (index > 0)
+                    {
+                        index--;
+                    }
+                    else
+                    {
+                        index = maxIndex;
+                    }
+                }
+                keyDown = true;
+            }
+        }
+        else
+        {
+            keyDown = false;
+        }
+        Debug.Log(index);
+    }
+}
