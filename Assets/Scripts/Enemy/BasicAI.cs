6f7011d7a5ea5157c0a238d1f07ad8d7f92bb804
Ewillian <guillaume.lecoq@ynov.com>
2021-03-05 20:02:26 +0100

    Enemy chase

--- Assets/Scripts/Enemy/BasicAI.cs
+++ Assets/Scripts/Enemy/BasicAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +13,13 @@ public class BasicAI : MonoBehaviour
 
     private Rigidbody2D rigibody;
 
+    private int moveDirection;
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    private bool is_facingRight = true;
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +32,57 @@ public class BasicAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        BaseAI();
+
+        Animate();
+    }
+
+    private void Animate()
+    {
+        Debug.Log(moveDirection);
+        if (moveDirection > 0 && !is_facingRight)
+        {
+            ChangeEnemyDirection();
+        }
+        else if (moveDirection < 0 && is_facingRight)
+        {
+            ChangeEnemyDirection();
+        }
+    }
+
+    private void BaseAI()
+    {
         float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
-        if ()
+        if (distanceToPlayer < agroRange)
         {
-
+            Chase();
         }
+    }
+
+    private void Chase()
+    {
+        if (transform.position.x < player.position.x)
+        {
+            // Enemy is on the left and need to move right
+            moveDirection = 1;
+            rigibody.velocity = new Vector2(moveSpeed, rigibody.velocity.y);
+        }
+        else if (transform.position.x > player.position.x)
+        {
+            // Enemy is on the right and need to move left
+            moveDirection = -1;
+            rigibody.velocity = new Vector2(-moveSpeed, rigibody.velocity.y);
+        }
+    }
+
+    /// <summary>
+    /// Using facingRight variable, change the character front rotation according to enemy movement.
+    /// </summary>
+    /// <returns></returns>
+    private void ChangeEnemyDirection()
+    {
+        is_facingRight = !is_facingRight; // Reverse bool
+        transform.Rotate(0f, 180f, 0f);
     }
 }
