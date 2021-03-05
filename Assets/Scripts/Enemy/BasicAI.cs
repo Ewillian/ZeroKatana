fc04e19109d805d998575bb7e6858af85e8f888f
Ewillian <guillaume.lecoq@ynov.com>
2021-03-05 18:59:18 +0100

    Based AI init

---
+++ Assets/Scripts/Enemy/BasicAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAI : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    private float agroRange;
+
+    private float moveSpeed;
+
+    private Rigidbody2D rigibody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigibody = GetComponent<Rigidbody2D>();
+        moveSpeed = 2;
+        agroRange = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if ()
+        {
+
+        }
+    }
+}
